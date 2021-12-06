43d5a45e98da32732be33d41d2d6213afe17a1aa
tan-andy <andytan@nevada.unr.edu>
2021-12-05 20:44:39 -0800

    revised_doxy

--- docs/html/search/all_0.js
+++ docs/html/search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['cs_20202_20semester_20project_2c_20f21_0',['CS 202 Semester Project, F21',['../d0/d30/md_README.html',1,'']]]
+  ['cs_20202_20semester_20project_2c_20f21_0',['CS 202 Semester Project, F21',['../index.html',1,'']]]
 ];
